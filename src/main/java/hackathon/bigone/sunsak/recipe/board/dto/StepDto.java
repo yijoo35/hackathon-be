568198b619607722726e0f575e35e957692c580c
yijoo <761610@naver.com>
2025-08-08 18:33:00 +0900

    ãrecipe fix

--- src/main/java/hackathon/bigone/sunsak/recipe/board/dto/StepDto.java
+++ src/main/java/hackathon/bigone/sunsak/recipe/board/dto/StepDto.java
@@ -2,11 +2,10 @@ package hackathon.bigone.sunsak.recipe.board.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
+
 @Getter
 @Setter
-public class BoardDto {
-    private String title;
-    private List<StepDto> steps;
-
+public class StepDto {
+    private int StepNumber;
+    private String description;
 }
--- src/main/java/hackathon/bigone/sunsak/recipe/board/entity/Board.java
+++ src/main/java/hackathon/bigone/sunsak/recipe/board/entity/Board.java
@@ -21,7 +21,7 @@ public class Board {
     private String title;
 
     @ManyToOne
-    @JoinColumn(name = "username")
+    @JoinColumn(name = "user_id")
     private SiteUser author;
 
 
