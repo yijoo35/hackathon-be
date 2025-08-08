7671ebdedcc9109565096ace76213fe9e5274623
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-08 16:23:11 +0900

    fix:modify yml

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,6 @@ pkill -f 'java -jar' || true
 
 echo ">>> Start new jar"
 JAR_NAME=$(ls build/libs/*SNAPSHOT.jar | grep -v plain | head -n 1)
-nohup java -jar "$JAR_NAME" > ../log.out 2>&1 &
+
+# 프로필 prod 설정 추가
+nohup SPRING_PROFILES_ACTIVE=prod java -jar "$JAR_NAME" > ../log.out 2>&1 &
